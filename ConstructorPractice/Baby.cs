bf7fefb2e5a1f37c31b64ab1dde3ae0497a62b3f
cemil3535 <cemilozcan35@gmail.com>
2024-08-07 19:39:54 +0300

    Add project files.

---
+++ ConstructorPractice/Baby.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorPractice
+{
+    internal class Baby
+    {
+        private string _name;
+        private string _surname;
+        private DateTime _dateTime;
+
+        // Default Constructor to identify
+        public Baby()
+        {
+            Console.WriteLine($"Ingaaaa {DateTime.Now}");
+        }
+
+
+        // Alternative to identify
+        public Baby(string name, string surname)
+        {
+            _name = name;
+            _surname = surname;
+            Console.WriteLine($"{_name} {_surname} Ingaaa {DateTime.Now}");
+            
+        }
+
+
+        // Properties Created
+        public string Name
+        {
+            get
+            {
+                return _name;
+
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+
+            }
+            set
+            {
+                _surname = value;
+            }
+
+        }
+
+        public DateTime DateTime
+        {
+            get
+            {
+                return _dateTime;
+            }
+            set
+            {
+                _dateTime = value;
+            }
+        }
+    }
+}
+
---
+++ ConstructorPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+ using ConstructorPractice;
+using System.Threading.Channels;
+public class Program 
+{ 
+    public static void Main(string[] args)
+    {
+        // The object is defined with the default constructor
+        Baby baby1 = new Baby();
+
+        Console.WriteLine("-----------------------------------------");
+
+        //The object is defined with a constructor that takes parameters.
+        Baby baby2 = new Baby("Ali", "Guzel"); 
+    }
+   
+}
+
+
+
